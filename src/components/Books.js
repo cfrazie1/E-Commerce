21b65612839bcdba489cd87d627d8b420041f65a
Curtis Frazier <fraziercurtis2003@yahoo.com>
2017-07-31 13:57:15 -0400

    Final Books and Entertainment

--- src/components/Books.js
+++ src/components/Books.js
@@ -2,6 +2,16 @@ import React, { Component } from 'react';
 import {NavLink} from 'react-router-dom';
 import '../styles/Books.css';
 import '../styles/BaseLayout.css';
+import books from '../imgs/books/BooksImg.jpeg';
+import TheArtofWar from '../imgs/books/TheArtofWar.png';
+import Laws from '../imgs/books/Laws.png';
+import Outliers from '../imgs/books/Outliers.png';
+import HarryPotter from '../imgs/books/HarryPotter.png';
+import LordofTheRings from '../imgs/books/LordofTheRings.png';
+import Goosebumps from '../imgs/books/Goosebumps.png';
+import Cat from '../imgs/books/Cat.png';
+import CuriousGeorge from '../imgs/books/curiousGeorge.png';
+import Matilda from '../imgs/books/Matilda.png';
 
 export default class Books extends Component {
   render() {
@@ -9,77 +19,93 @@ export default class Books extends Component {
       <div className='booksDiv'>
 
 
-          <div className='categoriesDiv'>
-           <h1>Non-Fiction</h1>
-            <div className='products'>
-              <img src="../img/product.jpg" />
-              <p>'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum mauris leo, ultricies nec aliquet sit amet, imperdiet id mi. '</p>
-            </div>
-            <div className='products'>
-              <img src="../img/product.jpg" />
-              <p>'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum mauris leo, ultricies nec aliquet sit amet, imperdiet id mi. '</p>
-            </div>
-            <div className='products'>
-              <img src="../img/product.jpg" />
-              <p>'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum mauris leo, ultricies nec aliquet sit amet, imperdiet id mi. ' </p>
-              <a href='#'>
-              my link
-              </a>
-            </div>
-           </div>
-
-
-          <div className='categoriesDiv'>
-           <h1>Sci-Fi</h1>
-           <div className='products'>
-            <img src="../img/product.jpg" />
-           <p>'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum mauris leo, ultricies     aliquet sit amet, imperdiet id mi. ' </p>
-           </div>
-           <div className='products'>
-            <img src="../img/product.jpg" />
-             <p>'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum mauris leo, ultricies nec aliquet sit amet, imperdiet id mi. ' </p>
-             <a href='#'>
-             my link
-             </a>
-           </div>
-           <div className='products'>
-            <img src="../img/product.jpg" />
-             <p>'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum mauris leo, ultricies nec aliquet sit amet, imperdiet id mi. ' </p>
-             <a href='#'>
-             my link
-             </a>
-           </div>
-
+        <div className='categoriesDiv'>
+          <h1>Non-Fiction</h1>
+          <div className='products'>
+            <img src={TheArtofWar} />
+            <p>'The Art of War'</p>
+            <h6>19.99</h6>
+            <a href='#'>
+              add to cart
+            </a>
           </div>
-
-
-
-          <div className='categoriesDiv'>
-           <h1>Children</h1>
-           <div className='products'>
-            <img src="../img/product.jpg" />
-           <p>'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum mauris leo, ultricies nec aliquet sit amet, imperdiet id mi. ' </p>
-           <a href='#'>
-           my link
-           </a>
-            </div>
-            <div className='products'>
-              <img src="../img/product.jpg" />
-              <p>'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum mauris leo, ultricies nec aliquet sit amet, imperdiet id mi. ' </p>
-              <a href='#'>
-              my link
-              </a>
-            </div>
-            <div className='products'>
-              <img src="../img/product.jpg" />
-              <p>'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum mauris leo, ultricies nec aliquet sit amet, imperdiet id mi. ' </p>
-              <a href='#'>
-              my link
-              </a>
-            </div>
-           </div>
-
+          <div className='products'>
+            <p>'48 Laws of Power'</p>
+            <img src={Laws}/>
+            <h6>9.99</h6>
+            <a href='#'>
+              add to cart
+            </a>
           </div>
+          <div className='products'>
+            <p>'Outliers'</p>
+            <img src={Outliers}/>
+            <h6>19.99</h6>
+            <a href='#'>
+              add to cart
+            </a>
+          </div>
+        </div>
+
+
+        <div className='categoriesDiv'>
+          <h1>Sci-Fi</h1>
+          <div className='products'>
+            <img src={HarryPotter}/>
+          <p>'Harry Potter'</p>
+          <h6>11.99</h6>
+          <a href='#'>
+            add to cart
+          </a>
+          </div>
+          <div className='products'>
+            <img src={LordofTheRings} />
+            <p>'Lord of the Rings'</p>
+            <h6>19.99</h6>
+            <a href='#'>
+              add
+            </a>
+          </div>
+          <div className='products'>
+            <img src={Goosebumps} />
+            <p>'Goosebumps'</p>
+            <h6>19.99</h6>
+            <a href='#'>
+              my
+            </a>
+          </div>
+        </div>
+
+
+
+        <div className='categoriesDiv'>
+          <h1>Children</h1>
+          <div className='products'>
+            <img src={Cat}/>
+            <p>'The Cat in the Hat '</p>
+            <h6>19.99</h6>
+            <a href='#'>
+              add to cart
+            </a>
+          </div>
+          <div className='products'>
+            <img src={Matilda}/>
+            <p>'Matilda'</p>
+            <h6>9.99</h6>
+            <a href='#'>
+              add to cart
+            </a>
+          </div>
+          <div className='products'>
+            <p>'Curious George'</p>
+            <img src={CuriousGeorge}/>
+            <h6>19.99</h6>
+            <a href='#'>
+              add to cart
+            </a>
+          </div>
+        </div>
+      </div>
     )
   }
 }
--- src/components/Entertainment.js
+++ src/components/Entertainment.js
@@ -2,53 +2,100 @@ import React, { Component } from 'react';
 import {NavLink} from 'react-router-dom';
 import '../styles/Entertainment.css';
 import '../styles/BaseLayout.css';
-
+import Samsung from '../imgs/entertainment/Samsung.png';
+import Insignia from '../imgs/entertainment/Insignia.png';
+import Sony from '../imgs/entertainment/Sony.png';
+import MacBook from '../imgs/entertainment/MacBook.png';
+import Dell from '../imgs/entertainment/Dell.png';
+import MacBook13 from '../imgs/entertainment/MacBook13.png';
+import DellPrinter from '../imgs/entertainment/DellPrinter.png';
+import HPOffice from '../imgs/entertainment/HPOffice.png';
+import HPPrinter from '../imgs/entertainment/HPPrinter.png';
 
 export default class Entertainment extends Component {
   render() {
     return(
-    <div className='entertainmentDiv'>
-
-
+      <div className='entertainmentDiv'>
         <div className='categoriesDiv'>
-         <h1>Televisions</h1>
+          <h1>Televisions</h1>
           <div className='products'>
-            <p>'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum mauris leo, ultricies nec aliquet sit amet, imperdiet id mi. Proin blandit pretium tincidunt. Morbi molestie neque id dui tincidunt aliquet. Vivamus malesuada magna et justo pellentesque ornare. Phasellus pellentesque nisi eros, a fermentum arcu scelerisque ut. Quisque iaculis neque urna, sed suscipit ex pellentesque pharetra. Mauris condimentum volutpat lobortis. Proin efficitur ex pharetra quam laoreet luctus quis at mi. Curabitur in massa ullamcorper, suscipit eros non, molestie neque. Nam at purus eget enim condimentum congue. Etiam et varius metus.'</p>
+            <p>'40 inch Samsung TV'</p>
+            <img src={Samsung} />
+            <h6>399.99</h6>
+            <a href='#'>
+              add to cart
+            </a>
           </div>
 
           <div className='products'>
-            <p>'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum mauris leo, ultricies nec aliquet sit amet, imperdiet id mi. Proin blandit pretium tincidunt. Morbi molestie neque id dui tincidunt aliquet. Vivamus malesuada magna et justo pellentesque ornare. Phasellus pellentesque nisi eros, a fermentum arcu scelerisque ut. Quisque iaculis neque urna, sed suscipit ex pellentesque pharetra. Mauris condimentum volutpat lobortis. Proin efficitur ex pharetra quam laoreet luctus quis at mi. Curabitur in massa ullamcorper, suscipit eros non, molestie neque. Nam at purus eget enim condimentum congue. Etiam et varius metus.'</p>
+            <p>'50 inch Vizio TV'</p>
+            <img src={Insignia} />
+            <h6>599.99</h6>
+            <a href='#'>
+              add to cart
+            </a>
           </div>
 
           <div className='products'>
-            <p>'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum mauris leo, ultricies nec aliquet sit amet, imperdiet id mi. Proin blandit pretium tincidunt. Morbi molestie neque id dui tincidunt aliquet. Vivamus malesuada magna et justo pellentesque ornare. Phasellus pellentesque nisi eros, a fermentum arcu scelerisque ut. Quisque iaculis neque urna, sed suscipit ex pellentesque pharetra. Mauris condimentum volutpat lobortis. Proin efficitur ex pharetra quam laoreet luctus quis at mi. Curabitur in massa ullamcorper, suscipit eros non, molestie neque. Nam at purus eget enim condimentum congue. Etiam et varius metus.'</p>
+            <p>'60 inch Sony TV'</p>
+            <img src={Sony} />
+            <h6>699.99</h6>
+            <a href='#'>
+              add to cart
+            </a>
           </div>
         </div>
 
         <div className='categoriesDiv'>
-         <h1>Computers</h1>
+          <h1>Computers</h1>
           <div className='products'>
+            <p>'15 inch MacBook Pro'</p>
+            <img src={MacBook}/>
+            <h6>999.99</h6>
+            <a>add to cart</a>
+          </div>
+
+          <div className='products'>
+            <p>'15 inch Dell Laptop'</p>
+            <img src={Dell}/>
+            <h6>199.99</h6>
+            <a>
+              add to cart
+            </a>
           </div>
           <div className='products'>
-          </div>
-          <div className='products'>
+            <p>'13 inch MacBook Pro'</p>
+            <img src={MacBook13}/>
+            <h6>699.99</h6>
+            <a>add to cart</a>
           </div>
         </div>
 
         <div className='categoriesDiv'>
-         <h1>Printers</h1>
+          <h1>Printers</h1>
           <div className='products'>
+            <p>'Dell Color Printer'</p>
+            <img src={DellPrinter}/>
+            <h6>199.99</h6>
+            <a>add to cart</a>
           </div>
           <div className='products'>
+            <p>'HP 3D Printer'</p>
+            <img src={HPPrinter}/>
+            <h6>199.99</h6>
+            <a href='#'>
+              add to cart
+            </a>
           </div>
           <div className='products'>
+            <p>'HP Office All in One Printer'</p>
+            <img src={HPOffice}/>
+            <h6>199.99</h6>
+            <a href='#'>
+              add to cart
+            </a>
           </div>
-        </div>
-
-
-
-
-
+          </div>
       </div>
     )
   }
